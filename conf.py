04a14bbb5184016dc22f0bb5f20880770829063e
YKPark <czeny@naver.com>
2021-01-19 11:48:19 +0900

    로고 추가

--- conf.py
+++ conf.py
@@ -175,5 +175,9 @@ epub_title = project
 # A list of files that should not be packed into the epub file.
 epub_exclude_files = ['search.html']
 
-
+html_theme_options = {
+    'logo_only': True,
+    # ...
+}
+html_logo = "C:\docbook\eformsignkr_developers\images\\forcs.png"
 
